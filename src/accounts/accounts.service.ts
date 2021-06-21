e2800a0d7aa4bd57f0722ebc9fe80e84175a065f
Alfredo Cano <alfrecanodiaz@gmail.com>
2021-06-21 17:52:37 -0400

    feat (accounts): validate account balance, update account balance

--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AccountEntity } from './entities/accounts.entity';
+import { TransactionEntity } from '../transactions/entities/transactions.entity';
+import { TransactionType } from '../transactions/transactions.types';
+import { TransactionCommitDTO } from '../transactions/dto/transactions.dto';
+import { ErrorsConstants } from '../constants/error.constants';
 import { AccountBalance } from './dto/accounts.dto';
 
 @Injectable()
@@ -24,5 +28,34 @@ export class AccountsService {
     if (!count) {
       await this.accountRepository.save({ balance: 0 });
     }
+  }
+
+  async checkAccountBalance(transaction: TransactionCommitDTO): Promise<void> {
+    const account = await this.accountRepository.findOne();
+
+    if (!account) {
+      throw ErrorsConstants.resourceNotFound('account');
+    }
+
+    if (
+      account.balance < transaction.amount &&
+      transaction.type === TransactionType.DEBIT
+    ) {
+      throw ErrorsConstants.insufficientAccountBalanceError();
+    }
+  }
+
+  async updateAccountBalance(transaction: TransactionEntity): Promise<void> {
+    const account = await this.accountRepository.findOne();
+
+    if (!account) {
+      throw ErrorsConstants.resourceNotFound('account');
+    }
+
+    account.balance =
+      transaction.type === TransactionType.CREDIT
+        ? account.balance + transaction.amount
+        : account.balance - transaction.amount;
+    await this.accountRepository.save(account);
   }
 }

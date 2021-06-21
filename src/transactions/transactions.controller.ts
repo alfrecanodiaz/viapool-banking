afdfceda09a3db1f91cab941a46ba0ac45e46552
Alfredo Cano <alfrecanodiaz@gmail.com>
2021-06-21 18:08:24 -0400

    feat (transactions): fetch transactions history

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,6 +1,25 @@
 import {
   Controller,
+  Get,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
+import {
+  ApiOperation,
+} from '@nestjs/swagger';
+import { TransactionEntity } from './entities/transactions.entity';
+import { TransactionsService } from './transactions.service';
 
 @Controller('transactions')
-export class TransactionsController {}
+export class TransactionsController {
+  constructor(private transactionsService: TransactionsService) {}
+
+  @Get('')
+  @UsePipes(ValidationPipe)
+  @ApiOperation({
+    summary: 'Fetches transactions history',
+  })
+  public async fetchTransactions(): Promise<TransactionEntity[]> {
+    return await this.transactionsService.fetchTransactions();
+  }
+}
--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { TransactionEntity } from './entities/transactions.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TransactionsService {}
+export class TransactionsService {
+  constructor(
+    @InjectRepository(TransactionEntity)
+    private readonly transactionRepository: Repository<TransactionEntity>,
+  ) {}
+
+  async fetchTransactions(): Promise<TransactionEntity[]> {
+    return await this.transactionRepository.find();
+  }
+}
